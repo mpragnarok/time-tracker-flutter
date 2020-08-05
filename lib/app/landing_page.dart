ed37bc8833170dc9b0796367a6831d59acc62f72
mina.h <mina.h@amaryllo.us>
2020-08-05 17:31:06 +0800

    <feat><Auth><Complete Dependency Injection of Auth
    Add Auth class for firebaseAuth

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home_page.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class LandingPage extends StatefulWidget {
+  LandingPage({@required this.auth});
+  final AuthBase auth;
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  User _user;
+  @override
+  void initState() {
+    super.initState();
+    _checkCurrentUser();
+  }
+
+  Future<void> _checkCurrentUser() async {
+    User user = await widget.auth.currentUser();
+    _updateUser(user);
+  }
+
+  void _updateUser(User user) {
+    setState(() {
+      _user = user;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_user == null) {
+      return SignInPage(
+        auth: widget.auth,
+//      Same as: onSignIn: (user) => _updateUser(user), onSignIn and _updateUser have the same signature(FirebaseUser)
+        onSignIn: _updateUser,
+      );
+    }
+    return HomePage(
+      auth: widget.auth,
+      onSignOut: () => _updateUser(null),
+    );
+  }
+}
---
+++ lib/common_widgets/custom_raised_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomRaisedButton extends StatelessWidget {
+  CustomRaisedButton({
+    this.child,
+    this.color,
+    this.borderRadius: 2.0,
+    this.onPressed,
+    this.height: 50.0,
+  }) : assert(borderRadius != null);
+  final Widget child;
+  final Color color;
+  final double borderRadius;
+  final double height;
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: RaisedButton(
+        onPressed: onPressed,
+        color: color,
+        child: child,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(borderRadius),
+          ),
+        ),
+      ),
+    );
+  }
+}
