6b4a91ec3efa017cf47904ab38b61689a5faabc4
mina.h <mina.h@amaryllo.us>
2020-08-06 16:38:21 +0800

    <refactor><landing page><statelessWidget>
    - Add StreamBuilder to landing page
    - Add onAuthStateChanged in auth
    - Refactor Landing page from StatefulWidget to StatelessWidget

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -3,54 +3,26 @@ import 'package:time_tracker_flutter_course/app/home_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
-class LandingPage extends StatefulWidget {
+class LandingPage extends StatelessWidget {
   LandingPage({@required this.auth});
   final AuthBase auth;
-  @override
-  _LandingPageState createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-  User _user;
-  @override
-  void initState() {
-    super.initState();
-    _checkCurrentUser();
-    widget.auth.onAuthStateChanged.listen((user) {
-//      if user is null it'll print "user: null"
-      print('user: ${user?.uid}');
-    });
-  }
-
-  Future<void> _checkCurrentUser() async {
-    User user = await widget.auth.currentUser();
-    _updateUser(user);
-  }
-
-  void _updateUser(User user) {
-    setState(() {
-      _user = user;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User>(
-        stream: widget.auth.onAuthStateChanged,
+        stream: auth.onAuthStateChanged,
         builder: (context, snapshot) {
-          snapshot.connectionState
-          if (snapshot.hasData) {
+//          .hasData is to check the data != null and before first stream emit, the snapshot doesn't have any data, this is the reason we change it to check the connectionState is activated
+
+          if (snapshot.connectionState == ConnectionState.active) {
             User user = snapshot.data;
             if (user == null) {
               return SignInPage(
-                auth: widget.auth,
-//      Same as: onSignIn: (user) => _updateUser(user), onSignIn and _updateUser have the same signature(FirebaseUser)
-                onSignIn: _updateUser,
+                auth: auth,
               );
             }
             return HomePage(
-              auth: widget.auth,
-              onSignOut: () => _updateUser(null),
+              auth: auth,
             );
           } else {
 //            Spinner
